987848dcc3b4c08a73fbc9fe7b7209318e5ee58f
Wagner Assis <wagner.czb@gmail.com>
2026-06-08 13:21:30 -0300

    chore: move source files out of app routes

--- app/src/models/Tournament.ts
+++
@@ -1,8 +0,0 @@
-import { Organization } from './Organization';
-
-export type Tournament = {
-  id: string;
-  name: string;
-  slug: string;
-  organization: Organization;
-};
--- app/src/navigation/types.ts
+++
@@ -1,8 +0,0 @@
-export type RootStackParamList = {
-  TournamentsScreen: undefined;
-  TournamentHistoryScreen: { id: string };
-  PlayoffsView: { tournamentId: string; phaseId?: string };
-  ClassificationView: { phaseId: string; tournamentId?: string };
-  GroupPhaseView: { phaseId: string; tournamentId?: string };
-  // ...other screens if any...
-};
